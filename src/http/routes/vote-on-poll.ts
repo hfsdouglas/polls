922d273b8099fcd3e6e501cba972c3ec719ceba1
Douglas Faria <hfsdouglas@gmail.com>
2024-02-13 16:02:58 -0300

    feat: create route vote-on-polls

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,33 @@
+import { randomUUID } from "crypto";
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+
+export async function voteOnPoll(app: FastifyInstance) {
+    app.post('/polls/:pollId/votes', (request, reply) => {
+        const voteOnPollBody = z.object({
+            pollOptionId: z.string().uuid()
+        })
+
+        const voteOnPollParams = z.object({
+            pollId: z.string().uuid()
+        })
+
+        const { pollId } = voteOnPollParams.parse(request.params)
+        const { pollOptionId } = voteOnPollBody.parse(request.body)
+
+        let { sessionId } = request.cookies
+
+        if (!sessionId) {
+            sessionId = randomUUID()
+
+            reply.setCookie('sessionId', sessionId, {
+                path: '/',
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                signed: true,
+                httpOnly: true
+            })
+        }
+
+        return reply.status(201).send({ sessionId })
+    })
+}
